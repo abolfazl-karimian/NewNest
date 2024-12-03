072eea9fcee58f0fa80acb9981e761f28f65aabe
“Abolfazl <abolfazl8karimian@gmail.com>
2024-12-03 20:45:23 +0330

    Add Dockerfile & run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
-mkdir "store-volume"
+install docker
+install git
+git clone https://github.com/abolfazl-karimian/NewNest.git
+cd NewNest
+chmod +x run.sh
+bash run.sh
+
+mkdir -p "store-volume"
 docker build -t newnest-app  .
 docker run --name newnest-app -v ./store-volume:/opt/nest/store -d newnest-app
