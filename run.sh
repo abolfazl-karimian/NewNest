950c73a04532f650e7c0779d043e10d871949cf9
“Abolfazl <abolfazl8karimian@gmail.com>
2024-12-03 20:49:02 +0330

    add conf files

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
-install docker
-install git
-git clone https://github.com/abolfazl-karimian/NewNest.git
-cd NewNest
-chmod +x run.sh
-bash run.sh
-
 mkdir -p "store-volume"
 docker build -t newnest-app  .
 docker run --name newnest-app -v ./store-volume:/opt/nest/store -d newnest-app
